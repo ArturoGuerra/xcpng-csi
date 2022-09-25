973af9cbe6a9f18a9521be9b6c3df5d588cb1789
shokre <kbrlek@gmail.com>
2022-09-25 23:47:03 +0200

    fix(xapi): fix attachment procedure

--- pkg/xapi/attach.go
+++ pkg/xapi/attach.go
@@ -10,53 +10,63 @@ import (
 // Attach attaches volume to node
 func (c *xClient) Attach(volID, nodeID, fstype string) (string, error) {
 	vdiRef := xoclient.VDIRef(volID)
-	vms, err := c.GetVMByName(nodeID)
-	if err != nil {
-		return "", err
-	}
 
-	if len(vms) > 1 || len(vms) == 0 {
-		return "", errors.New("Error fetching VMs")
-	}
-
-	vm := vms[0]
-
+	// retrieve requested VDI
 	vdi, err := c.GetVDIByUUID(vdiRef)
 	if err != nil {
 		return "", err
 	}
 
+	// retrieve requested node
+	vm, err := c.GetVMFromK8sNode(nodeID)
+	if err != nil {
+		return "", err
+	}
+
+	// retrieve VBDs for VDI
 	vbds, err := c.GetVBDsFromVDI(vdi.UUID)
 	if err != nil {
 		return "", err
 	}
 
-	for _, VBD := range vbds {
-		if err := c.DeleteVBD(VBD.UUID); err != nil {
-			log.Error(err)
+	// we have more than one VBD for VDI so something is wrong
+	if len(vbds) > 1 {
+		return "", errors.New("Too much VBDs, something is wrong")
+	}
+
+	// if we have no VBDs for VDI, we should attach
+	if len(vbds) == 0 {
+		// lets attach one
+		log.Infof("Attaching VDI: %s to VM: %s", vdi.UUID, vm.UUID)
+		if err = c.AttachVBD(vdi.UUID, vm.UUID); err != nil {
+			// if attach faild return error
+			return "", err
 		}
-	}
 
-	if err = c.AttachVBD(vdi.UUID, vm.UUID); err != nil {
-		return "", err
-	}
+		// TODO: maybe give some time for VBD to become available
+		// time.Sleep(5)
 
-	vbds, err = c.GetVBDsFromVDI(vdi.UUID)
-	if err != nil {
-		return "", err
-	}
-
-	if len(vbds) != 1 {
-		return "", fmt.Errorf("Found %d VBDs", len(vbds))
+		// restart attachment procedure
+		return "", errors.New("VDI attached successfully, waiting for VBD to become available")
 	}
 
 	var vbd *xoclient.VBD
 
-	for _, VBD := range vbds {
-		if VBD.VM == vm.UUID {
-			vbd = VBD
-		}
+	if vbds[0].VM == vm.UUID {
+		// found attachment on proper node
+		vbd = vbds[0]
 	}
 
-	return fmt.Sprintf("/dev/%s", vbd.Device), nil
+	if vbd == nil {
+		return "", errors.New("Failed to attach volume (no VBD)")
+	}
+
+	if vbd.Device == "" {
+		return "", errors.New("Failed to attach volume (no device)")
+	}
+
+	// return device to mount
+	dev := fmt.Sprintf("/dev/%s", vbd.Device)
+	log.Infof("Attached dev: %s, VDI: %s to VM: %s", dev, vdi.UUID, vm.UUID)
+	return dev, nil
 }
